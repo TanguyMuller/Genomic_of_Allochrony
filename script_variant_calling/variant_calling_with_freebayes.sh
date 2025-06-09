0feb28e0244eacdf1659e5f07be788e27656616d
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-06-09 17:40:20 +0200

    Create variant_calling_with_freebayes.sh

---
+++ script_variant_calling/variant_calling_with_freebayes.sh
@@ -0,0 +1,30 @@
+### Script for variant calling 
+
+#! /bin/bash
+
+# Set input variables
+BAMLIST=$1
+REGION=$2
+PATH_TO_ASSEMBLY=/path/to/assembly/PPM.fa
+
+# FreeBayes is slower than other variant callers
+# It is run on 1 Mb non-overlapping regions
+
+# Variant calling for autosomal regions
+freebayes -f $PATH_TO_ASSEMBLY -L $BAMLIST -r $REGION \
+  -E -1 --max-complex-gap -1 --haplotype-length -1 -K \
+  -C 1 -F 0.01 -G 1 --limit-coverage 500 -n 3 -m 30 -q 20 | \
+gzip -c > res_vcf/${REGION}.freebayes.vcf.gz
+
+# Variant calling for Z chromosome (haploid samples defined in sample.bed)
+# Note: -A assigns ploidy 1 to listed samples (e.g., females on chrZ)
+freebayes -f $PATH_TO_ASSEMBLY -L $BAMLIST -r $REGION \
+  -E -1 --max-complex-gap -1 --haplotype-length -1 -K \
+  -C 1 -F 0.01 -G 1 --limit-coverage 500 -n 3 -m 30 -q 20 \
+  -A sample.bed | \
+gzip -c > res_vcf/${REGION}.freebayes.vcf.gz
+
+### Command to run variant_calling_with_freebayes.sh
+
+# Note : the format of the regions.chunks file is chr:start-end
+#awk '{print "sbatch -J FBch"FNR" --output FBch"FNR" variant_calling_with_freebayes.sh portugal.bam.list",$1}' regions.chunks
