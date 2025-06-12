102f7210f29058b064a29c20fd4c581b5816694b
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-06-12 23:40:34 +0200

    Create concat.sh

---
+++ script_variant_calling/concat.sh
@@ -0,0 +1,6 @@
+### Script for concat vcf file 
+
+#! /bin/bash
+
+# Note : Chunk were ordered by chromosome and position  in vcf.freebayes.list
+bcftools concat -f vcf.freebayes.list | bcftools view -S sample.ord - -Oz > portugal.freebayes.vcf.gz
