aa73d4a422f77cb814401b1912c252cf139de426
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-06-25 20:26:55 +0200

    Create 8_concat_gVCF_ind_portugal.sh

---
+++ script_variant_calling/8_concat_gVCF_ind_portugal.sh
@@ -0,0 +1,6 @@
+### Script for concat vcf file 
+
+#! /bin/bash
+
+# Note : gVCF files are ordered by chromosome in gvcf.ind.portugal.list
+bcftools concat -f gvcf.ind.portugal.list | bcftools view -S sample.ord - -Oz > gVCF.portugal.vcf.gz
