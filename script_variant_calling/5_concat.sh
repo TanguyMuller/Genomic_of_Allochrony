4776d11c0df6b267601fedd3fb57b3b767a0961d
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-06-25 15:11:19 +0200

    Update 5_concat.sh

--- script_variant_calling/5_concat.sh
+++ script_variant_calling/5_concat.sh
@@ -2,7 +2,10 @@
 
 #! /bin/bash
 
-# Note : Chunk were ordered by chromosome and position in all.vcf.freebayes.list, pool.vcf.freebayes.list and ind.vcf.freebayes.list. This concatenation script in only for autosomes VCF.
+# Note : Chunk were ordered by chromosome and position in all the lists. 
 bcftools concat -f all.vcf.freebayes.list | bcftools view -S all.sample.ord - -Oz > all.portugal.freebayes.vcf.gz
 bcftools concat -f pool.vcf.freebayes.list | bcftools view -S pool.sample.ord - -Oz > pool.portugal.freebayes.vcf.gz
 bcftools concat -f ind.vcf.freebayes.list | bcftools view -S ind.sample.ord - -Oz > ind.portugal.freebayes.vcf.gz
+bcftools concat -f all.vcf.freebayes.chrZ.list | bcftools view -S all.sample.ord - -Oz > all.portugal.freebayes.chrZ.vcf.gz
+bcftools concat -f pool.vcf.freebayes.chrZ.list | bcftools view -S pool.sample.ord - -Oz > pool.portugal.freebayes.chrZ.vcf.gz
+bcftools concat -f ind.vcf.freebayes.chrZ.list | bcftools view -S ind.sample.ord - -Oz > ind.portugal.freebayes.chrZ.vcf.gz
