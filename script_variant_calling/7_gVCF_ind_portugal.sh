93f28d65be7b7a94c3b504aa4149af329297388c
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-06-25 20:19:25 +0200

    Create 7_gVCF_ind_portugal.sh

---
+++ script_variant_calling/7_gVCF_ind_portugal.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+BAMLIST=$1
+REGION=$2
+PATH_TO_ASSEMBLY=/PATH/TO/ASSEMBLY
+
+bcftools mpileup -Ou -C 50 --max-depth 500 -q 20 -Q 20 -a DP,AD -f $PATH_TO_ASSEMBLY -b $BAMLIST -r $REGION | bcftools call -m -Oz -o gVCF_Portugal/Portugal.${REGION}.gVCF.vcf.gz
+
+### Command to run 7_gVCF_ind_portugal.sh
+#awk '{print "bash 7_gVCF_ind_portugal.sh ind.bam.list ",$1}' chr.list | bash
