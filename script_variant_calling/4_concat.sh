75a8efef37f0ec4a24eff954e3beafc53d31273b
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-06-25 15:00:43 +0200

    Update 4_concat.sh

--- script_variant_calling/4_concat.sh
+++ script_variant_calling/4_concat.sh
@@ -2,7 +2,7 @@
 
 #! /bin/bash
 
-# Note : Chunk were ordered by chromosome and position in all.vcf.freebayes.list, pool.vcf.freebayes.list and ind.freebayes.list. This concatenation script in only for autosomes VCF.
+# Note : Chunk were ordered by chromosome and position in all.vcf.freebayes.list, pool.vcf.freebayes.list and ind.vcf.freebayes.list. This concatenation script in only for autosomes VCF.
 bcftools concat -f all.vcf.freebayes.list | bcftools view -S all.sample.ord - -Oz > all.portugal.freebayes.vcf.gz
 bcftools concat -f pool.vcf.freebayes.list | bcftools view -S pool.sample.ord - -Oz > pool.portugal.freebayes.vcf.gz
 bcftools concat -f ind.vcf.freebayes.list | bcftools view -S ind.sample.ord - -Oz > ind.portugal.freebayes.vcf.gz
