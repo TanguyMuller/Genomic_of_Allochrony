aa1ac4546d82464f9ff0dff75274821cba428659
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-06-25 14:53:44 +0200

    Update 4_concat.sh

--- script_variant_calling/4_concat.sh
+++ script_variant_calling/4_concat.sh
@@ -3,4 +3,6 @@
 #! /bin/bash
 
 # Note : Chunk were ordered by chromosome and position in vcf.freebayes.list
-bcftools concat -f vcf.freebayes.list | bcftools view -S sample.ord - -Oz > all.portugal.freebayes.vcf.gz
+bcftools concat -f all.vcf.freebayes.list | bcftools view -S all.sample.ord - -Oz > all.portugal.freebayes.vcf.gz
+bcftools concat -f pool.vcf.freebayes.list | bcftools view -S pool.sample.ord - -Oz > pool.portugal.freebayes.vcf.gz
+bcftools concat -f ind.vcf.freebayes.list | bcftools view -S ind.sample.ord - -Oz > ind.portugal.freebayes.vcf.gz
