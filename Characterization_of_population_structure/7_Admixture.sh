da7f723ab4d1741ac8eddb49ab0f0e760820dcbe
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-07-04 13:53:59 +0200

    Update 7_Admixture.sh

--- Characterization_of_population_structure/7_Admixture.sh
+++ Characterization_of_population_structure/7_Admixture.sh
@@ -13,4 +13,4 @@ admixture --cv $base_name.bed $nbr_grp > log${nbr_grp}.out
 
 ### Command to run 7_Admixture.sh
 #for i in {2..10}; do echo "sbatch 7_Admixture.sh ind.portugal.freebayes_filt_maf005_ldprune.vcf.gz $i Auto" ; done | bash
-#for i in {2..10}; do echo "sbatch 7_Admixture.sh ind.portugal.freebayes_filt_maf005_ldprune.vcf.gz $i chrZ" ; done | bash
+#for i in {2..10}; do echo "sbatch 7_Admixture.sh ind.portugal.freebayes.chrZ_filt_maf005_ldprune.vcf.gz $i chrZ" ; done | bash
