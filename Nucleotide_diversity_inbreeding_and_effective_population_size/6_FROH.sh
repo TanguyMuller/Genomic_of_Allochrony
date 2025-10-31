8cb9d577dc5bba0cf316281e15110c49454bca89
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-10-31 18:45:55 +0100

    Create 6_FROH.sh

---
+++ Nucleotide_diversity_inbreeding_and_effective_population_size/6_FROH.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script for run FROH analyses for ROH>100kb and ROH>500Kb 
+
+for size in "100" "500";do
+	for pop in "LSP" "LWP" "FU" "VI" "VA" "CA" "TA" "GR";do
+		mkdir -p ${size}kb
+		VCF_INPUT=""
+		OUTPUT="${size}kb/${pop}.ROH.${size}kb"
+		
+		./plink --allow-extra-chr \
+				--chr-set 49 \
+				--double-id \
+				--homozyg \
+				--homozyg-density 20 \
+				--homozyg-gap 100 \
+				--homozyg-kb $size \
+				--homozyg-snp 50 \
+				--homozyg-window-het 2 \
+				--homozyg-window-missing 5 \
+				--homozyg-window-snp 50 \
+				--out $OUTPUT \
+				--vcf $VCF_INPUT
+	done;
+done
