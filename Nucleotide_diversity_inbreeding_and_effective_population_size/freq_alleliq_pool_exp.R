60f31e4c6196a5d9ad96e433053cf68ec742c166
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-10-31 18:08:24 +0100

    Update freq_alleliq_pool_exp.R

--- Nucleotide_diversity_inbreeding_and_effective_population_size/freq_alleliq_pool_exp.R
+++ Nucleotide_diversity_inbreeding_and_effective_population_size/freq_alleliq_pool_exp.R
@@ -9,18 +9,10 @@ vcf_base <- args[1]
 vcf_path <- paste0("/your/path/", vcf_base, ".vcf.gz")
 
 # Charger les échantillons
-sample.list <- as.character(read.table("/path/to/list/pool_list.list")[,1])
-# Vérifier si la liste est bien chargée
-if (length(sample.list) == 0) {
-  stop("ERREUR : La liste des pools est vide.")
-}
+sample.list <- as.character(read.table("/path/to/list/pool.portugal.list")[,1])
 
 # Définir les tailles de pool
-poolsizes <- c(52, 80, 80, 80, 80, 80, 80, 62, 78, 100, 80, 80, 68)
-# Vérifier la cohérence entre poolsizes et sample.list
-if (length(poolsizes) != length(sample.list)) {
-  stop("ERREUR : Nombre de poolsizes différent du nombre d'échantillons.")
-}
+poolsizes <- c(100,100,80,40,52,70,74,70,34,32)
 
 # Appliquer vcf2pooldata
 tmp <- vcf2pooldata(
@@ -30,23 +22,16 @@ tmp <- vcf2pooldata(
   min.cov.per.pool = 10
 )
 
-
-output_name <- gsub("_pool", "", vcf_base)
-output_name <- gsub("\\.vcf\\.gz$", "", output_name)
-
 # Créer le dataframe d'informations
 info <- as.data.frame(cbind(V1 = tmp@snp.info$Chromosome, V2 = tmp@snp.info$Position))
 
 # Écrire le fichier de sortie avec le nom correct
 write.table(info[,1:2], 
-            paste0("/work/project/loadexp/analyses_tanguy/Exp/list/positions_", output_name, "_10covperpool.txt"), 
+            paste0("/your/path/to/list/positions_", vcf_base, "_10covperpool.txt"), 
             quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
 
-# Liste des noms de pools (correspond aux colonnes dans le VCF)
-pops <- c("ES_COR", "FR_BEA", "FR_BOR", "FR_BRE", "FR_CHA", "FR_ERQ", "FR_FON", "FR_LES", "FR_MON", "FR_ORL", "FR_QUI", "FR_VEN", "IT_TRE")
-
 # Boucle sur chaque pool
-for (i in seq_along(pops)) {
+for (i in seq_along(sample.list)) {
   pop <- pops[i]
   # Calculer la fréquence pour le pool actuel
   freq <- as.data.frame(tmp@refallele.readcount[,i] / tmp@readcoverage[,i])
