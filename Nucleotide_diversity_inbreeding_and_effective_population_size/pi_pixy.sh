c0606fb98b7f34b76cbd8ea6a70c2f50b295df7d
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-10-31 16:10:50 +0100

    Update pi_pixy.sh

--- Nucleotide_diversity_inbreeding_and_effective_population_size/pi_pixy.sh
+++ Nucleotide_diversity_inbreeding_and_effective_population_size/pi_pixy.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
 ### Calcul of Pi using pixy
-### First bgzip the gVCF and index with tabix command
-for i in ind.gVCF.vcf.gz; do
+# Extraire le nom du chromosome
+chr=$(basename "$i" | cut -d'.' -f2)
+mkdir -p /home/mullerta/work/SP_WP/Analyses/Nucleotide_diversity/"$chr"
+# Indexer le fichier VCF
+tabix -p vcf "${i}"
 
-   # Extraire le nom du chromosome
-   chr=$(basename "$i" | cut -d'.' -f2)
-
-   # Créer un dossier pour le chromosome si ce n'est pas déjà fait
-   mkdir -p /home/mullerta/work/SP_WP/Analyses/Nucleotide_diversity/"$chr"
-
-   # Indexer le fichier VCF
-   tabix -p vcf "${i}"
-
-   # Exécuter la commande pixy
-   pixy --stats pi \
+# Exécuter la commande pixy
+pixy --stats pi \
    --vcf "${i}" \
    --populations /home/mullerta/work/SP_WP/Analyses/Nucleotide_diversity/population_list/population.portugal.list \
    --window_size 100000 \
