28b68a003f287b80a4f8263be6dd4697594cbf38
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-10-31 17:56:32 +0100

    Update 4_freq_alleliq_pool.sh

--- Nucleotide_diversity_inbreeding_and_effective_population_size/4_freq_alleliq_pool.sh
+++ Nucleotide_diversity_inbreeding_and_effective_population_size/4_freq_alleliq_pool.sh
@@ -1,35 +1,33 @@
 #!/bin/bash 
-# Script to filter SNPs without 10 reads per pool
-# and estimate the allele frequency from pools
+# Script to filter SNPs with fewer than 10 reads per pool
+# and to estimate allele frequencies from pool data
 # for RZooRoH analyses.
 #
 # Command
-# bash 4_freq_alleliq_pool.sh ind.vcf.gz Auto
+# bash 4_freq_alleliq_pool.sh ind.vcf.gz 
 
 vcf=$1
 path_list="/your/path/to/list"
 path_script="/your/path/to/script"
+base_name=$(basename "$vcf" .vcf.gz)
 
-
-# Set directory for RZooRoH
+# Prepare output directories
 mkdir -p freq_alleliq
 mkdir -p results
 mkdir -p GL
 
-# Last filt on min_cov_per_pool=10 
-# Create freq alleliq on pool for each population
-Rscript ${path_script}/freq_alleliq_pool_exp.R ${dataset}
+# Run the R script that computes allele frequencies per pool
+echo "Estimating allele frequencies for ${vcf}..."
+Rscript ${path_script}/freq_alleliq_pool_exp.R ${vcf}
 
-# Keep only autosomes SNPs
-grep -v chrZ ${path_list}/positions_${base_name}_10covperpool.txt > tmp.list
-mv tmp.list ${path_list}/positions_${base_name}_10covperpool.txt
+# Remove sites with fewer than 10 reads per pool
+echo "Filtering SNPs with at least 10 reads per pool..."
+bcftools view -T ${path_list}/positions_${vcf}_10covperpool.txt -Oz ${vcf} > ${base_name}_10covperpool.vcf.gz
+bcftools index ${base_name}_10covperpool.vcf.gz
 
-bcftools view -T ${path_list}/positions_${base_name}_10covperpool.txt -Oz ${path_vcf}/${base_name}.vcf.gz > ${path_vcf}/${base_name}_10covperpool.vcf.gz
-bcftools index ${path_vcf}/${base_name}_10covperpool.vcf.gz
-#rm ${path_vcf}/${dataset}*
+# Number of SNPs retained for RZooRoH analyses
+num_snps=$(gunzip -c "${base_name}_10covperpool.vcf.gz" | grep -c ^chr)
+echo "Number of SNPs retained for RZooRoH analyses: ${num_snps}"
 
-#if [[ "$dataset" != "chr49_filt_0pct_missing_dp_filtered_pool.vcf.gz" ]]; then
-#    rm -f ${path_vcf}/${base_name}.vcf.gz*
-#fi
-
-gunzip -c ${path_vcf}/${base_name}_10covperpool.vcf.gz | grep -c ^c
+echo "Filtered VCF created: ${base_name}_10covperpool.vcf.gz"
+echo "Process completed successfully"
