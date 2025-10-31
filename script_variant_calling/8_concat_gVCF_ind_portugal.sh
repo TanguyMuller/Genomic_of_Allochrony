bdb108e081bc4d78d26a437d8e7e577d61648680
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-10-31 16:14:40 +0100

    Update 8_concat_gVCF_ind_portugal.sh

--- script_variant_calling/8_concat_gVCF_ind_portugal.sh
+++ script_variant_calling/8_concat_gVCF_ind_portugal.sh
@@ -1,4 +1,6 @@
-#! /bin/bash
 ### Script to concat gvcf file with portuguese individuals 
+
+#! /bin/bash
+
 # Note : gVCF files are ordered by chromosome in gvcf.ind.portugal.list
 bcftools concat -f gvcf.ind.portugal.list | bcftools view -S sample.ord - -Oz > gVCF.portugal.vcf.gz
