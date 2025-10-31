e700e42aba59b314764cc48b4a19d6c25004626b
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-10-31 18:59:12 +0100

    Update 7_create_map_and_ped.sh

--- Nucleotide_diversity_inbreeding_and_effective_population_size/7_create_map_and_ped.sh
+++ Nucleotide_diversity_inbreeding_and_effective_population_size/7_create_map_and_ped.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 # Script for create ped and map file for GONe analyses
+# and for currentNe2
 
+mkdir -p vcf
 pop=$1
 
-VCF="${pop}_ind_masked_HO.cvf.gz"
-#bcftools view -T ^list.position.N.txt vcf/${VCF} -Oz > vcf/masked.${VCF}
-plink --vcf vcf/masked.${VCF} --recode --out vcf/masked.chrA.${pop}.pruned_mono.MAF --chr-set 49 --allow-extra-chr --double-id --mac 1
-awk '{print $1,NR,$3,$4}' vcf/masked.chrA.${pop}.pruned_mono.MAF.map > vcf/tmp.masked.chrA.${pop}.pruned_mono.MAF.map
-mv vcf/masked.chrA.${pop}.pruned_mono.MAF.map vcf/masked.chrA.${pop}.pruned_mono.MAF.map.unchanged
-mv vcf/tmp.masked.chrA.${pop}.pruned_mono.MAF.map vcf/masked.chrA.${pop}.pruned_mono.MAF.map 
+VCF="${pop}_ind_masked_HWE.vcf.gz"
+plink --vcf ${VCF} --recode --out vcf/${pop}_prune_mono --chr-set 49 --allow-extra-chr --double-id --mac 1
+awk '{print $1,NR,$3,$4}' vcf/${pop}.pruned_mono.map > vcf/tmp.${pop}.pruned_mono.MAF.map
+mv vcf/${pop}.pruned_mono.map vcf/${pop}.pruned_mono.map.unchanged
+mv vcf/tmp.${pop}.pruned_mono.MAF.map vcf/${pop}.pruned_mono.map
