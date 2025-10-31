38dbb89fc137c8fa733e56a5d59b35ef8e712dd9
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-10-31 16:17:36 +0100

    Update 10_concat_gVCF_divergence.sh

--- script_variant_calling/10_concat_gVCF_divergence.sh
+++ script_variant_calling/10_concat_gVCF_divergence.sh
@@ -1,4 +1,6 @@
+### Script to concat gvcf file with the 2 most covered individuals of LSP, LWP, FU and outgroups
+
 #! /bin/bash
-### Script to concat gvcf file divergence
+
 # Note : gVCF files are ordered by chromosome in gvcf.divergence.portugal.list
 bcftools concat -f gvcf.divergence.portugal.list | bcftools view -S sample.ord - -Oz > gVCF.divergence.vcf.gz
