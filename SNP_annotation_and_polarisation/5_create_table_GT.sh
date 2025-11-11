6acb75d46ec83923aa3aa974279b640ff4a4bae5
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-11-11 23:00:31 +0100

    Create 5_create_table_GT.sh

---
+++ SNP_annotation_and_polarisation/5_create_table_GT.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Extract the fields for next load analyses
+
+# Parameters
+VCF=$1
+OUT=$2
+
+# Run
+#freebayes:
+java -jar snpEff/SnpSift.jar extractFields -e "NA" vcf/${VCF} "CHROM" "POS" "REF" "ALT" "GEN[*].GT" "ANN[0].EFFECT" "ANN[0].IMPACT" "ANN[0].HGVS_P" | sed 's/,/\t/g' > vcf/${OUT}
+
+#Command
+#sbatch 5_create_table_GT.sh load.ann.vcf.gz load.ann.GT.txt
