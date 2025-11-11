72e864d0c126d3d30ca133f91ede62d933737def
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-11-11 23:52:41 +0100

    Update 4_launcher_snpEff.sh

--- SNP_annotation_and_polarisation/4_launcher_snpEff.sh
+++ SNP_annotation_and_polarisation/4_launcher_snpEff.sh
@@ -3,3 +3,6 @@
 
 java -Xmx4g -jar snpEff/snpEff.jar PPM demo.portugal.vcf.gz | bgzip -c > load.ann.vcf.gz
 tabix -p vcf load.ann.vcf.gz
+
+java -Xmx4g -jar snpEff/snpEff.jar PPM Z.demo.portugal.vcf.gz | bgzip -c > Z_load.ann.vcf.gz
+tabix -p vcf Z_load.ann.vcf.gz
