d9ea5c4194623ab13a97c43206d9ee81c8723fa7
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-11-12 15:11:23 +0100

    Update 1_create_table_GT.sh

--- Estimation_of_genetic_load/1_create_table_GT.sh
+++ Estimation_of_genetic_load/1_create_table_GT.sh
@@ -12,3 +12,4 @@ java -jar snpEff/SnpSift.jar extractFields -e "NA" vcf/${VCF} "CHROM" "POS" "REF
 #Command
 #sbatch 1_create_table_GT.sh load.ann.vcf.gz load.ann.GT.txt
 #sbatch 1_create_table_GT.sh Z.load.ann.vcf.gz Z.load.ann.GT.txt
+#sbatch 1_create_table_GT.sh load.all.ann.vcf.gz load.all.ann.GT.txt
