1588c62ed60c02113353a225f414822b617c855f
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-11-12 15:13:28 +0100

    Update 4_launcher_snpEff.sh

--- SNP_annotation_and_polarisation/4_launcher_snpEff.sh
+++ SNP_annotation_and_polarisation/4_launcher_snpEff.sh
@@ -6,3 +6,7 @@ tabix -p vcf load.ann.vcf.gz
 
 java -Xmx4g -jar snpEff/snpEff.jar PPM Z.demo.portugal.vcf.gz | bgzip -c > Z_load.ann.vcf.gz
 tabix -p vcf Z_load.ann.vcf.gz
+
+# For HoN/HoS ratios and espected heterozygosity
+java -Xmx4g -jar snpEff/snpEff.jar PPM ind.portugal.vcf.gz | bgzip -c > load.all.ann.vcf.gz
+tabix -p vcf load.all.ann.vcf.gz
