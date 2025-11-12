26951e112de1247ca4ad3160c48a7186478ee93a
TanguyMuller <162686651+TanguyMuller@users.noreply.github.com>
2025-11-12 18:50:50 +0100

    Update 5_pi_pixy_Z.sh

--- Search_for_genomic_bases_associated_with_allochrony/5_pi_pixy_Z.sh
+++ Search_for_genomic_bases_associated_with_allochrony/5_pi_pixy_Z.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# Script to calculate Pi for the Z chromosome for LSP and LWP males
 
-
+# gCVF file
 gvcf="gVCF.portugal.vcf.gz"
 
 bcftools -S SP_WP_males.list -r chrZ ${gvcf} -Oz > Z.SPWP.males.gVCF.vcf.gz
 tabix -p vcf Z.SPWP.males.gVCF.vcf.gz
 
-# Exécuter la commande pixy
+# Run pixy to calculate nucleotide diversity (Pi)
+# Note: SP_WP_males_pi.list must be in 'ind<TAB>pop' format
 pixy --stats pi \
      --vcf Z.SPWP.males.gVCF.vcf.gz \
      --populations SP_WP_males_pi.list \
